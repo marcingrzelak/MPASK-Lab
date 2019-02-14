68b3459d978400d693dc33f62772c609d25bd6b5
Marcin Grzelak <marcin.grzelak@op.pl>
2019-02-14 12:57:36 +0100

    Auto stash before merge of "task6" and "origin/task6"

---
+++ SNMPClient/Exceptions.h
@@ -0,0 +1,36 @@
+#pragma once
+class Exceptions
+{
+public:
+	virtual void message() = 0;
+};
+
+class eSocketInitialization : public Exceptions
+{
+public:
+	void message();
+};
+
+class eSocketCreate : public Exceptions
+{
+public:
+	void message();
+};
+
+class eSocketBind : public Exceptions
+{
+public:
+	void message();
+};
+
+class eSocketListening : public Exceptions
+{
+public:
+	void message();
+};
+
+class eSocketConnect : public Exceptions
+{
+public:
+	void message();
+};
---
+++ SNMPClient/Strings.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#pragma region errors
+constexpr auto SOCKET_INIT_ERR = "Blad inicjalizacji socketa\n\n";
+constexpr auto SOCKET_CREATE_ERR = "Blad podczas tworzenia socketa\n\n";
+constexpr auto SOCKET_BIND_ERR = "Blad - socket bind\n\n";
+constexpr auto SOCKET_LISTEN_ERR = "Blad - sluchanie na sockecie\n\n";
+constexpr auto SOCKET_CONNECT_ERR = "Socket - blad polaczenia \n\n";
+#pragma endregion
+
+
+#pragma region messages
+constexpr auto CLIENT_PDU_ENTER = "Wpisz pakiet PDU\n";
+#pragma endregion
